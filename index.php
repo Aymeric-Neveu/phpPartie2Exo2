57b63d084c8d8a2f4f617e7c906968720f399da9
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 16:19:12 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $isEasy = true;
+    if ($isEasy == true) {
+      echo "cest facile!!!";
+    }
+    elseif ($isEasy == false) {
+      echo "Cest difficile!!!";
+    }
+    ?>
+  </body>
